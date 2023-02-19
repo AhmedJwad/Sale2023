384d82d23a715c5b4d1ad5cc99084dcf506f893c
AhmedJwad <Ahmednet380@gmail.com>
2023-02-19 10:15:39 -0800

    Add country

---
+++ Sale.Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sale.Shared.Entities;
+
+namespace Sale.Api.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Country>
+    }
+}
